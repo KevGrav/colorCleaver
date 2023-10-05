2c1d8872aec4a4ded9fe2e1936249660ae94c5d7
KevGrav <kgravell.prsvr@gmail.com>
2023-10-05 10:20:18 -0400

    changes after class review

--- color-cleaver.js
+++ color-cleaver.js
@@ -9,16 +9,20 @@ const getInput = require("./get-input.js");
 let color1 = getInput(1)
 let color2 = getInput(2)
 // Is there 2 colors?
-if(color2) {
-    if(isValidPrimary){
-        return(colorCombinator(color1, color2))
-      } else { // Is there one color?
-        if(color1 >0) {
-            if(isValidSecondary){
-                return(colorDeconstructor(color1))
-            } else {
-            console.log('We need at least one color')        
-        } 
-      }
+if(color2 !== undefined) {
+    if(isValidPrimary(color1.toLowerCase()) && isValidPrimary(color2.toLowerCase()) && color1 !== color2){
+        console.log(colorCombinator(color1, color2))
+    } else {
+        console.log('Colors must be two different valid primary colors')
     }
- }
+}
+else if(color1 !== undefined){
+    if(isValidSecondary(color1.toLowerCase())){
+        console.log(colorDeconstructor(color1))
+    } else {
+        console.log('Color must be a valid secondary color')      
+    } 
+     
+}else {
+    console.log('We need at least one valid color')  
+}
